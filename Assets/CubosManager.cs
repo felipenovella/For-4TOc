4feeb49f5a0d5f06f8d835bc39f74ae28c6a6920
felipenovella <49439231@est.ort.edu.ar>
2025-06-17 09:06:00 -0300

    fin clase 17/6

---
+++ Assets/CubosManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubosManager : MonoBehaviour
+{
+    public CuboScript[] cuboScripts;
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < cuboScripts.Length; i++)
+        {
+            Debug.Log(cuboScripts[i].EsPersistente);
+            if (cuboScripts[i].EsPersistente != true)
+            {
+                Destroy(cuboScripts[i].gameObject);
+            }
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
